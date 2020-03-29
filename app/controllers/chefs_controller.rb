faca7c72d1a4a4d3150d9c5654925e2ea9515fcf
wuh11 <hui.wu@vumc.org>
2020-03-28 21:42:52 -0500

    Edit chef

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -14,6 +14,19 @@ class ChefsController < ApplicationController
     def show
         @chef = Chef.find(params[:id])
     end
+    
+    def edit
+        @chef = Chef.find(params[:id])
+    end
+    def update
+        @chef = Chef.find(params[:id])
+        if @chef.update(chef_params)
+            flash[:sucess] = "Your account was updated successfully"
+            redirect_to @chef
+        else
+            render 'edit'
+        end
+    end
     private
     def chef_params
         params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com",
+            password: "password", password_confirmation: "password")
+  end
+  test "reject an invalid edit" do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: " ", email: "mashrur@example.com" }} #update
+    assert_template 'chefs/edit' #error
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+  test 'accept valid edit' do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: "mashrur1", email: "mashrur1@example.com" }} #update
+    assert_redirected_to @chef #redirect_to
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "mashrur1", @chef.chefname
+    assert_match "mashrur1@example.com", @chef.email
+  end 
+end
