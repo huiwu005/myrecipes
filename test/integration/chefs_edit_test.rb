69ba11621be6aba9718778ad90ca479d1bad6b84
wuh11 <hui.wu@vumc.org>
2020-03-30 14:29:06 -0500

    complete auth system and tests

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -6,6 +6,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
             password: "password", password_confirmation: "password")
   end
   test "reject an invalid edit" do
+    sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
     patch chef_path(@chef), params: { chef: { chefname: " ", email: "mashrur@example.com" }} #update
@@ -14,6 +15,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.panel-body'
   end
   test 'accept valid edit' do
+    sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
     patch chef_path(@chef), params: { chef: { chefname: "mashrur1", email: "mashrur1@example.com" }} #update
